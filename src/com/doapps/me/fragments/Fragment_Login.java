62cefb02c8e531a3631c2a4664087cf3041d2a6b
Cristian Palomino Rivera <Gantz@MacBook-Pro-de-Cristian.local>
2014-05-17 13:00:57 -0500

    update

--- src/com/doapps/me/fragments/Fragment_Login.java
+++ src/com/doapps/me/fragments/Fragment_Login.java
@@ -1,15 +1,29 @@
 package com.doapps.me.fragments;
 
-import com.doapps.me.smily.R;
+import java.util.Arrays;
+import java.util.List;
 
+import android.app.Dialog;
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.doapps.me.smily.Main;
+import com.doapps.me.smily.R;
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseFacebookUtils;
+import com.parse.ParseUser;
 
 public class Fragment_Login extends Fragment {
 
+	private Dialog progressDialog;
+	
 	public final static Fragment_Login newInstance(){
 		return new Fragment_Login();
 	}
@@ -17,20 +31,26 @@ public class Fragment_Login extends Fragment {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-	}
+		}
 
-	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
-		return inflater.inflate(R.layout.fragment_login,container,false);
-	}
+		@Override
+		public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
+			return inflater.inflate(R.layout.fragment_login,container,false);
+		}
 
-	@Override
-	public void onPause() {
-		super.onPause();
-	}
+		@Override
+		public void onPause() {
+			super.onPause();
+		}
 
-	@Override
-	public void onResume() {
-		super.onResume();
+		@Override
+		public void onResume() {
+			super.onResume();
+		}
+
+		@Override
+		public void onActivityResult(int requestCode, int resultCode, Intent data) {
+			super.onActivityResult(requestCode, resultCode, data);
+			ParseFacebookUtils.finishAuthentication(requestCode, resultCode, data);
+		}
 	}
-}
